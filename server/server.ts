f4d2541a6025ecb99fc4711764b262a8c1b97972
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-08 20:30:10 -0700

    socketio + test html page

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import type { Express } from "express";
-import { createServer } from "node:http";
+import { createServer } from "http";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app: Express = express();
 const PORT: number = 3001;
 
 const server = createServer(app); // get the underlying server for socketIO -- to get low level access
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(__dirname, "../public")))
 
 server.listen(PORT, () => {
     console.log(`Running on port: ${PORT}`);
 })
 
 app.get("/", (req, res) => {
-    res.json({"message" : "Hello"});
-    console.log("Root hit");
+    res.sendFile(join(__dirname, "index.html"));
 }) 
