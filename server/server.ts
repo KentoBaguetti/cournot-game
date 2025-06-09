50f7a53d2656569ea297b3ca9c2e2dc73829846a
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-09 02:55:59 -0700

    fix something wrong with the db

--- server/server.ts
+++ server/server.ts
@@ -11,63 +11,59 @@ import { open } from "sqlite";
 
 // open database file
 const db = await open({
-    filename: "chat.db",
-    driver: sqlite3.Database
+  filename: "chat.db",
+  driver: sqlite3.Database,
 });
 
 await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-  );
-`);
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
 
 const app: Express = express();
 const PORT: number = 3001;
 const server = createServer(app); // get the underlying server for socketIO -- to get low level access
 const io = new Server(server, {
-    connectionStateRecovery: {} // store temp events to try and restore state when client reconnects
+  connectionStateRecovery: {}, // store temp events to try and restore state when client reconnects
 }); // create the socket.oi server wihth the low-level access server
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(join(__dirname, "../public")))
+app.use(express.static(join(__dirname, "../public")));
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Running on port: ${PORT}`);
-})
+  console.log(`Running on port: ${PORT}`);
+});
 
 // test socket.io connection
-io.on("connection", (socket) => {
-    socket.on("chat message", async (msg) => {
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      return;
+    }
+    io.emit("chat message", msg, result.lastID);
+  });
 
-        let result;
-
-        try {
-            result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
-        } catch (e) {
-            return;
+  if (!socket.recovered) {
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
         }
-
-        io.emit("chat message", msg, result.lastID);
-
-        if (!socket.recovered) {
-             // if the connection state recovery was not successful
-            try {
-            await db.each('SELECT id, content FROM messages WHERE id > ?',
-                [socket.handshake.auth.serverOffset || 0],
-                (_err, row) => {
-                socket.emit('chat message', row.content, row.id);
-                }
-            )
-            } catch (e) {
-            // something went wrong
-            }
-        }
-
-    })
-})
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "public/index.html"));
-}) 
+  res.sendFile(join(__dirname, "public/index.html"));
+});
