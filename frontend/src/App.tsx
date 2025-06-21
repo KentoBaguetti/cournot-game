deb7d71cf481d30d6a00cdc1818d8984d336fb20
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-21 01:18:31 -0700

    import management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { SocketProvider } from "./socket";
-import Home from "./pages/Home";
-import StudentJoin from "./pages/Student/StudentJoin";
-import InstructorLogin from "./pages/Instructor/InstructorLogin";
-import JanKenPoPage from "./pages/Games/JanKenPoPage";
 
-// non test shit
+// Both
+import Home from "./pages/Home";
+
+// Instructor pages
+import InstructorLogin from "./pages/Instructor/InstructorLogin";
 import InstructorDashboard from "./pages/Instructor/InstructorDashboard";
 import CreateJankenpoGamePage from "./pages/Instructor/CreateJankenpoGamePage";
 import GameDashboard from "./pages/Instructor/GameDashboard";
+
+// Student pages
+import StudentJoin from "./pages/Student/StudentJoin";
+import JanKenPoPage from "./pages/Games/JanKenPoPage";
 import GameLobby from "./pages/Student/GameLobby";
 
 function App() {
