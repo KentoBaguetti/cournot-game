64a22194f728fac365afaf4cf7fc3fb5b48830ae
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-26 01:29:04 -0700

    cournot game formulas

--- server/src/utils/cournotFormulas.ts
+++ server/src/utils/cournotFormulas.ts
@@ -13,10 +13,56 @@ const calculateMarketPrice = (
   return Math.max(0, res); // so we dont have a negative pruice
 };
 
-const calculateProfit = (revenue: number, cost: number): number => {
-  return revenue - cost;
+// caluculates the sum of the quantities in the array
+const calculateQuantitySum = (quantities: number[]): number => {
+  let qSum = 0;
+  for (const q of quantities) {
+    qSum += q;
+  }
+  return qSum;
 };
 
-const costFunction = () => {};
+const priceFunction = (x: number, quantities: number[]): number => {
+  const qSum = calculateQuantitySum(quantities);
+  return x - qSum;
+};
 
-export { calculateMarketPrice, calculateProfit };
+const costFunction = (y: number, z: number, quantities: number[]): number => {
+  const qSum = calculateQuantitySum(quantities);
+  return y + z * qSum;
+};
+
+// for a firm given that their quantity is the variable "quantity"
+// the array "quantities" contaisn the quantities of all the firms including the firm calling the function
+const profitFunction = (
+  x: number,
+  y: number,
+  z: number,
+  quantity: number,
+  quantities: number[]
+): number => {
+  const price = priceFunction(x, quantities);
+  const cost = costFunction(y, z, quantities);
+  return quantity * price - quantity * cost;
+};
+
+// the "quantities" array should not contain the quantity of the firm calling the function
+// calculates q_n, where q_n is the quantity of the firm calling the function
+// this function uses the derivative of the profit function with respect to q_n and sets it to 0, then solves for q_n
+const maxProfitFunction = (
+  x: number,
+  y: number,
+  z: number,
+  quantities: number[]
+): number => {
+  const qSum = calculateQuantitySum(quantities);
+  return (y + qSum - x) / (2 * z - 2);
+};
+
+export {
+  calculateMarketPrice,
+  priceFunction,
+  costFunction,
+  profitFunction,
+  maxProfitFunction,
+};
