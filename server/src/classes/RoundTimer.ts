6993a4f3066b4769943fa4a529d725ba4b188485
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-29 13:02:12 -0700

    init timer interval

--- server/src/classes/RoundTimer.ts
+++ server/src/classes/RoundTimer.ts
@@ -1,2 +1,6 @@
 // this will be made later, currently timer logic is handled by the games directly, but moving them into a class is better longe term
-export class RoundTimer {}
+export class RoundTimer {
+  //private TimerInterval: NodeJS.Timeout;
+
+  constructor() {}
+}
